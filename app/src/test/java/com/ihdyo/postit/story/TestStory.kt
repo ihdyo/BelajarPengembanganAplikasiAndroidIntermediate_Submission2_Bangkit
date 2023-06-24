fc96bc50ab10a3c8c7a5c5d45a8df7fab3b0fbc3
ihdyo <yodhianugrah23@gmail.com>
2023-06-24 14:29:39 +0700

    unitTesting

--- app/src/test/java/com/ihdyo/postit/story/TestStory.kt
+++ app/src/test/java/com/ihdyo/postit/story/TestStory.kt
@@ -5,10 +5,10 @@ import com.ihdyo.postit.data.db.DataDetail
 class TestStory {
     companion object{
         fun story(): List<DataDetail> {
-            val story1 = DataDetail("story-id12345678","Raihan", "Test Desctipyion", "https://story-api.dicoding.dev/images/stories/photos-1641623658595_dummy-pic.png", "2023-05-10T06:34:18.598Z", -10.212, -16.002)
-            val story2 = DataDetail("story-id41249291","Chaira", "Test Desctipyion", "https://story-api.dicoding.dev/images/stories/photos-1641623658595_dummy-pic.png", "2023-05-10T06:34:18.598Z", -10.212, -16.002)
-            val story3 = DataDetail("story-id22149129","Raihan", "Test Desctipyion", "https://story-api.dicoding.dev/images/stories/photos-1641623658595_dummy-pic.png", "2023-05-10T06:34:18.598Z", -10.212, -16.002)
-            val story4 = DataDetail("story-id04021232","Chaira", "Test Desctipyion", "https://story-api.dicoding.dev/images/stories/photos-1641623658595_dummy-pic.png", "2023-05-10T06:34:18.598Z", -10.212, -16.002)
+            val story1 = DataDetail("0001","Hello", "Description", "https://dicoding-web-img.sgp1.cdn.digitaloceanspaces.com/original/academy/dos:924dcc7e3d877a61292899b4c4ca8f1820220124100739.jpeg", "2023-06-10T06:34:18.598Z", -4.466667, 135.199997)
+            val story2 = DataDetail("0002","Hai", "Description", "https://dicoding-web-img.sgp1.cdn.digitaloceanspaces.com/original/academy/dos:924dcc7e3d877a61292899b4c4ca8f1820220124100739.jpeg", "2023-06-10T06:34:18.598Z", -4.466667, 135.199997)
+            val story3 = DataDetail("0003","Aloha", "Description", "https://dicoding-web-img.sgp1.cdn.digitaloceanspaces.com/original/academy/dos:924dcc7e3d877a61292899b4c4ca8f1820220124100739.jpeg", "2023-06-10T06:34:18.598Z", -4.466667, 135.199997)
+            val story4 = DataDetail("0004","Anyeong", "Description", "https://dicoding-web-img.sgp1.cdn.digitaloceanspaces.com/original/academy/dos:924dcc7e3d877a61292899b4c4ca8f1820220124100739.jpeg", "2023-06-10T06:34:18.598Z", -4.466667, 135.199997)
             return listOf(story1, story2, story3, story4)
         }
     }
